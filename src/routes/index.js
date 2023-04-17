280424710a58735c19efd14f16ca1e71f662804c
wize-ta.an <levanandtb05111999@gmail.com>
2023-04-17 23:38:05 +0700

    complete refactor structure ❤❤❤

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,9 @@
 const newsRouter = require("./news");
+const siteRouter = require("./site");
 
 function route(app) {
   app.use("/news", newsRouter);
-
-  app.get("/", (req, res) => {
-    res.render("home");
-  });
-
-  app.get("/search", (req, res) => {
-    console.log(req.query);
-    res.render("search");
-  });
+  app.use("/", siteRouter);
 }
 
 module.exports = route;
